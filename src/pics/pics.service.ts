576991125e3a51b4bd040a88779468eb8538e9af
Huy Hoang Phan <hoangph271@gmail.com>
2020-12-26 22:18:49 +0700

    [👌] Add onModuleInit() to pics service

--- src/pics/pics.service.ts
+++ src/pics/pics.service.ts
@@ -1,15 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DbService } from '../db.service';
 import { Repository } from 'typeorm';
 import { Pic } from './pics.entity';
+import { Env } from '../utils/env';
 
 @Injectable()
-export class PicsService extends DbService<Pic> {
+export class PicsService extends DbService<Pic> implements OnModuleInit {
   constructor(
     @InjectRepository(Pic)
     picRepository: Repository<Pic>,
   ) {
     super(picRepository);
+  }
+
+  async onModuleInit() {
+    if (Env.isNOTDev()) return;
+
+    await this.DANGEROUS_deleteAll();
   }
 }
