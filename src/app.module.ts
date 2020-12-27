2fb26b0b7a298c7c6bf3a0325e00a424f0df3554
Huy Hoang Phan <hoangph271@gmail.com>
2020-12-27 16:45:20 +0700

    [💩] Add missing app.module.ts imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { User } from './users/users.entity';
-import { Post } from './posts/posts.entity';
+import { Album } from './albums/albums.entity';
 import { Pic } from './pics/pics.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { PostsModule } from './posts/posts.module';
+import { AlbumsModule } from './albums/albums.module';
 import { PicsModule } from './pics/pics.module';
 
 const dbConfigs: TypeOrmModuleOptions = {
   type: 'sqlite',
   database: ':memory:',
   synchronize: true,
-  entities: [User, Post, Pic],
+  entities: [User, Album, Pic],
 };
 @Module({
   imports: [
     TypeOrmModule.forRoot(dbConfigs),
     UsersModule,
     AuthModule,
-    PostsModule,
+    AlbumsModule,
     PicsModule,
   ],
   providers: [],
