4b7c198885aed33269cd550128776a4678575db5
Huy Hoang Phan <hoangph271@gmail.com>
2020-12-27 12:55:34 +0700

    [👌] GET pics/ & pics/raw

--- src/pics/pics.controller.ts
+++ src/pics/pics.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+import * as path from 'path'
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { PicsService } from './pics.service';
 
 @Controller('pics')
-export class PicsController {}
+export class PicsController {
+  constructor(private picsService: PicsService) {}
+  @Get(':uuid')
+  @UseGuards(JwtAuthGuard)
+  async findPic(@Param('uuid') uuid: string) {
+    const pic = await this.picsService.findOne(uuid);
+
+    if (!pic) throw new NotFoundException();
+
+    return pic;
+  }
+
+  @Get('raw/:uuid')
+  @UseGuards(JwtAuthGuard)
+  async readRawPic(@Param('uuid') uuid: string, @Res() res: Response) {
+    // TODO: Stream file down to client
+    const pic = await this.picsService.findOne(uuid);
+
+    if (!pic) throw new NotFoundException();
+
+    const rs = await this.picsService.createReadStream(pic);
+
+    if (!rs) throw new NotFoundException();
+
+    const imageType = path.extname(pic.filePath).slice(1)
+    res.setHeader('Content-Type', `image/${imageType}`)
+    rs.pipe(res);
+  }
+}
