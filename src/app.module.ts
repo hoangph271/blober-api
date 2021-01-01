283e8082dfdea33b3a6a46e49e1a0c046a0d3971
Huy Hoang Phan <hoangph271@gmail.com>
2021-01-01 23:45:02 +0700

    [👌] Private albums

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,14 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { User } from './users/users.entity';
-import { Album } from './albums/albums.entity';
-import { Blob } from './blobs/blobs.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { AlbumsModule } from './albums/albums.module';
 import { BlobsModule } from './blobs/blobs.module';
-import { AlbumPic } from './albums/albums.pic.entity';
-import { DATABASE } from './utils/env';
 import { TasksModule } from './tasks/tasks.module';
 
-const dbConfigs: TypeOrmModuleOptions = {
-  type: 'sqlite',
-  database: DATABASE,
-  synchronize: true,
-  entities: [User, Album, Blob, AlbumPic],
-};
 @Module({
   imports: [
-    TypeOrmModule.forRoot(dbConfigs),
+    TypeOrmModule.forRoot(),
     UsersModule,
     AuthModule,
     AlbumsModule,
