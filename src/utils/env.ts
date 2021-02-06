eb277ff3d39182aeee9945ae8c8586ce07a1c98a
Huy Hoang Phan <hoangph271@gmail.com>
2021-02-07 03:51:52 +0700

    [🍑] Minor fix

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,8 +3,10 @@ export enum APP_STAGES {
 }
 
 export const { USE_MEMORY_DB, SQLITE_DB_FILE = 'blober.db' } = process.env
-export const { ALBUM_DIR, POST_DATA_FILE } = process.env
-export const FILE_DIRS = process.env.FILE_DIRS.split(',')
+export const { ALBUM_DIR, POST_DATA_FILE = '' } = process.env
+
+const { FILE_DIRS: FILE_DIRS_STR = '' } = process.env
+export const FILE_DIRS = FILE_DIRS_STR.split(',')
 
 export const HASH_ROUNDS = 12
 export const STAGE = APP_STAGES.DEV
