7f4cc979921c7307017e069d847dd0da5e530b85
Huy Hoang Phan <hoangph271@gmail.com>
2021-02-19 22:12:35 +0700

    [🍑] /files use _id now

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,4 +1,11 @@
-import { Headers, HttpStatus, Query, UseGuards } from '@nestjs/common'
+import * as path from 'path'
+import {
+  Headers,
+  HttpStatus,
+  InternalServerErrorException,
+  Param,
+  UseGuards,
+} from '@nestjs/common'
 import { NotFoundException } from '@nestjs/common'
 import { Res } from '@nestjs/common'
 import { Controller, Get } from '@nestjs/common'
@@ -10,39 +17,47 @@ import { FilesService, FSItem } from './files.service'
 export class FilesController {
   constructor(private filesService: FilesService) {}
 
-  @Get()
+  @Get(':_id?')
   @UseGuards(JwtAuthGuard)
-  async getItem(@Query('path') itemPath?: string): Promise<FSItem[] | FSItem> {
-    if (!itemPath) {
-      return this.filesService.readRoot()
-    }
+  async getItem(@Param('_id') _id = ''): Promise<FSItem> {
+    const fsItem = await this.filesService.readPath(_id)
 
-    return this.filesService.readPath(itemPath)
-  }
-
-  @Get('raw')
-  @UseGuards(JwtAuthGuard)
-  async getItemBinary(
-    @Query('path') itemPath: string,
-    @Headers() headers,
-    @Res() res: Response,
-  ) {
-    if (!itemPath) {
+    if (!fsItem) {
       throw new NotFoundException()
     }
 
-    const mime = await this.filesService.getContentType(itemPath)
+    return fsItem
+  }
+
+  @Get('raw/:_id?')
+  @UseGuards(JwtAuthGuard)
+  async getItemBinary(
+    @Headers() headers,
+    @Res() res: Response,
+    @Param('_id') _id = '',
+  ) {
+    const fullPath = await this.filesService.findPath(_id)
+
+    if (!fullPath) {
+      throw new NotFoundException()
+    }
+
+    const mime = await this.filesService.getContentType(fullPath)
     res.setHeader('Content-Type', mime)
+    res.setHeader(
+      'Content-Disposition',
+      `attachment; filename="${encodeURIComponent(path.basename(fullPath))}"`,
+    )
 
     const { range = '' } = headers
 
     if (!range) {
-      const rs = this.filesService.getFileReadable(itemPath)
+      const rs = this.filesService.getFileReadable(fullPath)
       rs.pipe(res)
       return
     }
 
-    const { size } = await this.filesService.getStats(itemPath)
+    const { size } = await this.filesService.getStats(fullPath)
     const parts = range.replace(/bytes=/, '').split('-')
     const start = Number.parseInt(parts[0], 10)
     const end = parts[1] ? Number.parseInt(parts[1], 10) : size - 1
@@ -53,18 +68,27 @@ export class FilesController {
     res.setHeader('Content-Length', chunksize)
     res.status(HttpStatus.PARTIAL_CONTENT)
 
-    const rs = this.filesService.getFileReadable(itemPath, { start, end })
+    const rs = this.filesService.getFileReadable(fullPath, { start, end })
     rs.pipe(res)
   }
 
-  @Get('preview')
+  @Get('preview/:_id?')
   @UseGuards(JwtAuthGuard)
-  async getItemPreview(@Query('path') itemPath: string, @Res() res: Response) {
-    if (!itemPath) {
+  async getItemPreview(@Res() res: Response, @Param('_id') _id = '') {
+    const fullPath = await this.filesService.findPath(_id)
+
+    if (!fullPath) {
       throw new NotFoundException()
     }
 
-    const { rs, mime } = await this.filesService.previewStream(itemPath)
+    const { rs, mime } = await this.filesService
+      .previewStream(fullPath)
+      .catch(() => {
+        throw new InternalServerErrorException()
+      })
+
+    if (!rs) throw new NotFoundException()
+
     res.setHeader('Content-Type', mime)
     rs.pipe(res)
   }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,56 +1,142 @@
 import * as path from 'path'
 import * as fs from 'fs/promises'
+import * as fsSync from 'fs'
+import * as crypto from 'crypto'
+import { Readable } from 'stream'
 import { createReadStream } from 'fs'
 import * as fileType from 'file-type'
 import { spawn } from 'child_process'
-import { Injectable } from '@nestjs/common'
+import * as fsWalk from '@nodelib/fs.walk'
+import { Injectable, Logger } from '@nestjs/common'
 import { FS_ROOTS } from '../utils/env'
-import { Readable } from 'stream'
 
-const fsEntries = FS_ROOTS.map((entry) => {
-  return {
-    itemPath: entry,
-    isDir: true,
-  }
+const CACHE_DIR = path.join('.fs_cache')
+fs.access(CACHE_DIR, fsSync.constants.F_OK).catch(() => {
+  fs.mkdir(CACHE_DIR)
 })
 
+const hash = (inStr: string) =>
+  crypto.createHash('sha256').update(inStr).digest('hex')
+
+const getRelativePath = (fullPath: string) => {
+  const rootPath = FS_ROOTS.find((root) => fullPath.startsWith(root))
+
+  return fullPath.replace(rootPath, '')
+}
+
+const fsRoot: FSItem = {
+  _id: '',
+  fullPath: '',
+  path: '',
+  mime: '',
+  isDir: true,
+  children: FS_ROOTS.map((path) => {
+    return {
+      _id: hash(path),
+      path,
+      isDir: true,
+      fullPath: path,
+    }
+  }),
+}
+
+type FSWalkEntry = {
+  name: string
+  path: string
+}
 export type FSItem = {
-  itemPath: string
+  _id: string
+  path: string
+  fullPath: string
+  mime?: string
+  size?: number
   isDir: boolean
   children?: FSItem[]
 }
 
 @Injectable()
 export class FilesService {
-  readRoot(): FSItem[] {
-    return fsEntries
-  }
+  async findPath(_id: string): Promise<string | undefined> {
+    const fsItem = fsRoot.children.find((item) => item._id === _id)
 
-  async readPath(itemPath: string): Promise<FSItem[] | FSItem> {
-    const stats = await fs.stat(itemPath)
+    if (fsItem) {
+      return fsItem.fullPath
+    }
+
+    for (const fsItem of fsRoot.children) {
+      const walkStream = fsWalk.walkStream(fsItem.fullPath)
+
+      for await (const entry of walkStream) {
+        const { path } = entry as FSWalkEntry
+
+        if (_id === hash(path)) return path
+      }
+    }
+  }
+  async readPath(_id: string): Promise<FSItem | undefined> {
+    if (_id === '') {
+      return fsRoot
+    }
+
+    const fullPath = await this.findPath(_id)
+
+    if (!fullPath) return
+
+    const stats = await fs.stat(fullPath)
 
     if (stats.isFile()) {
       return {
+        _id: hash(fullPath),
+        fullPath,
+        path: getRelativePath(fullPath),
         isDir: false,
-        itemPath,
-        ...(await fileType.fromFile(itemPath)),
+        mime: await this.getContentType(fullPath),
+        ...(await fileType.fromFile(fullPath)),
       }
     }
 
-    const childNames = await fs.readdir(itemPath)
-
-    return Promise.all(
+    const childNames = await fs.readdir(fullPath)
+    const children: FSItem[] = await Promise.all(
       childNames.map(async (childName) => {
-        const childPath = path.join(itemPath, childName)
-        const stats = await fs.stat(childPath)
+        const childPath = path.join(fullPath, childName)
 
-        return {
-          isDir: stats.isDirectory(),
-          itemPath: childPath,
-          ...(await fileType.fromFile(childPath)),
+        try {
+          const stats = await fs.stat(childPath)
+
+          const fsItem: FSItem = {
+            _id: hash(childPath),
+            fullPath: childPath,
+            path: getRelativePath(childPath),
+            isDir: stats.isDirectory(),
+            ...(stats.isDirectory()
+              ? { mime: '' }
+              : await fileType.fromFile(childPath)),
+          }
+
+          return fsItem
+        } catch (error) {
+          Logger.error(error)
+          const fsItem: FSItem = {
+            mime: '',
+            isDir: false,
+            _id: hash(fullPath),
+            fullPath: childPath,
+            path: getRelativePath(childPath),
+          }
+
+          return fsItem
         }
       }),
     )
+
+    return {
+      _id: hash(fullPath),
+      mime: '',
+      fullPath,
+      path: getRelativePath(fullPath),
+      children,
+      isDir: true,
+    }
   }
 
   async getStats(itemPath: string) {
@@ -72,47 +158,74 @@ export class FilesService {
   async previewStream(
     itemPath: string,
   ): Promise<{
-    rs: Readable
+    rs: Readable | null
     mime: string
   }> {
     const stats = await fs.stat(itemPath)
 
     if (stats.isDirectory()) {
-      return {
-        rs: createReadStream(path.join('icons', 'folder.svg')),
-        mime: 'image/svg+xml',
-      }
+      return { rs: null, mime: '' }
     }
 
     const { mime = '' } = (await fileType.fromFile(itemPath)) || {}
 
     switch (true) {
-      case mime.startsWith('video/'): {
+      case mime.startsWith('image/'): {
         return {
-          rs: this._videoPreviewStream(itemPath),
+          rs: await fsSync.createReadStream(itemPath),
+          mime,
+        }
+      }
+      case mime.startsWith('video/'): {
+        const rs = await this._videoPreviewStream(itemPath)
+
+        return {
+          rs: rs ?? null,
           mime: 'image/png',
         }
       }
       default: {
         return {
-          rs: createReadStream(path.join('icons', 'file-binary.svg')),
-          mime: 'image/svg+xml',
+          rs: null,
+          mime: null,
         }
       }
     }
   }
 
-  _videoPreviewStream(itemPath: string) {
-    return spawn('ffmpeg', [
-      '-i',
-      itemPath,
-      '-ss',
-      '00:00:01.000',
-      '-vframes',
-      '1',
-      '-f',
-      'image2',
-      'pipe:1',
-    ]).stdout
+  async _videoPreviewStream(
+    itemPath: string,
+  ): Promise<fsSync.ReadStream | null> {
+    const base64Path = Buffer.from(itemPath).toString('base64')
+    const cachedPath = path.join(CACHE_DIR, base64Path)
+    const existsCached = await fs
+      .access(cachedPath, fsSync.constants.R_OK)
+      .then(() => true)
+      .catch(() => false)
+
+    if (existsCached) {
+      return fsSync.createReadStream(cachedPath)
+    }
+
+    return new Promise((resolve) => {
+      spawn('ffmpeg', [
+        '-i',
+        itemPath,
+        '-ss',
+        '00:00:01.000',
+        '-vframes',
+        '1',
+        '-f',
+        'image2',
+        cachedPath,
+      ]).once('close', (code) => {
+        if (code === 0) {
+          resolve(fsSync.createReadStream(cachedPath))
+        } else {
+          console.error(`${itemPath}: ffmpeg failed with code ${code}`)
+          resolve(null)
+        }
+      })
+    })
   }
 }
